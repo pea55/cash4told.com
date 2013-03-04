879c066f01f1240d355ebb101a0ee907dc321df9
Samuel Ryan <sam@samryan.co.uk>
2013-03-04 22:23:10 +0000

    initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>cash4told.com</title>
+    <meta name="description" content="cash4told"/>
+    <script type="text/javascript">
+        
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-29439846-18']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    
+    </script>
+    <style type="text/css">
+        body{padding:10px 0 0 40px;}h1{font-size:60px;margin-bottom:0px;}h1, h6{color:#556270; font-family:Arial,Helvetica,sans-serif;}a{font-weight:bold;text-decoration:none;color:#8398B8;}a:hover{color:#F9023C;}
+    </style>
+</head>
+<body>
+    <div id="wrapper">
+        <h1><?php $lines = json_decode(file_get_contents("lines.json"), true); echo $lines[array_rand($lines)]; ?></h1>
+        <h6>Submit changes and view the full list via the <a href="https://github.com/citricsquid/cash4told.com/blob/master/lines.json">repository on github.com</a>.</h6>
+    </div>
+</body>
+</html>
