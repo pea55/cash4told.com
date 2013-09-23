46c037ebe49f825453ab7327856d04b34c1fb8df
Samuel Ryan <sam@samryan.co.uk>
2013-09-23 07:29:38 +0100

    toggleable copy pasta

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <title>cash4told.com</title>
-    <meta name="description" content="cash4told"/>
+    <meta name="description" content="cash4told, cash4told.com, told copypasta"/>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
     <script type="text/javascript">
         
     var _gaq = _gaq || [];
@@ -18,13 +19,19 @@
     
     </script>
     <style type="text/css">
-        body{padding:10px 0 0 40px;}h1{font-size:60px;margin-bottom:0px;}h1, h6{color:#556270; font-family:Arial,Helvetica,sans-serif;}.refresh{color:#28A76A; font-weight: bold;}a{font-weight:bold;text-decoration:none;color:#8398B8;}a:hover{color:#F9023C;}
+        body{padding:10px 0 0 40px; margin-bottom:40px;}h1{font-size:60px;margin-bottom:0px;}h1, h6{color:#556270; font-family:Arial,Helvetica,sans-serif;}.refresh{color:#28A76A; font-weight: bold;}.pasta{color:#AFBF49; text-decoration:underline;}a{font-weight:bold;text-decoration:none;color:#8398B8;}a:hover{color:#F9023C;}#copypasta{display:none;}
     </style>
+    <script type="text/javascript">
+        function show(div) {
+            document.getElementById(div).style.display = 'block';
+        }
+    </script>
 </head>
 <body>
     <div id="wrapper">
         <h1><?php $lines = json_decode(file_get_contents("lines.json"), true); echo $lines[array_rand($lines)]; ?></h1>
-        <h6><span class="refresh">Refresh for another!</span> Submit changes and view the full list via the <a href="https://github.com/citricsquid/cash4told.com/blob/master/lines.json">repository on github.com</a>.</h6>
+        <h6><span class="refresh">Refresh for another</span> / <a class="pasta" href="#" onClick="show('copypasta'); return false;">Click here to show copypasta</a> / Submit changes and view the full list via the <a href="https://github.com/citricsquid/cash4told.com/blob/master/lines.json">repository on github.com</a>.</h6>
+        <pre id="copypasta"><?php foreach($lines as $line) { echo "[X] " . $line . "<Br/>"; } ?></pre>
     </div>
 </body>
 </html>
